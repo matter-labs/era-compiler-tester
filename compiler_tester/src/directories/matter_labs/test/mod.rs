10f72c4bc0a06011c9e864e12cd75d7b155bc0cc
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-06-08 08:28:08 +0200

    feat: remove EVM interpreter runtime code repeats

--- compiler_tester/src/directories/matter_labs/test/mod.rs
+++ compiler_tester/src/directories/matter_labs/test/mod.rs
@@ -262,8 +262,7 @@ impl MatterLabsTest {
         let mut instances = BTreeMap::new();
 
         for (instance, evm_contract) in self.metadata.evm_contracts.iter() {
-            let instruction_name = instance.split('_').next().expect("Always exists");
-            let runtime_code = evm_contract.runtime_code(instruction_name);
+            let runtime_code = evm_contract.runtime_code();
             let mut bytecode = evm_contract.init_code(runtime_code.len());
             bytecode.push_str(runtime_code.as_str());
 
