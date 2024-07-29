d10fbaacbae1d76779b039ff2a895f3dbc1bdc6c
Javier Chatruc <jrchatruc@gmail.com>
2024-07-29 17:15:29 -0300

    Use consistent importing

--- compiler_tester/src/test/case/input/mod.rs
+++ compiler_tester/src/test/case/input/mod.rs
@@ -20,7 +20,7 @@ use std::sync::Mutex;
 use crate::compilers::mode::Mode;
 use crate::directories::matter_labs::test::metadata::case::input::Input as MatterLabsTestInput;
 use crate::summary::Summary;
-use crate::target;
+use crate::target::Target;
 use crate::test::instance::Instance;
 use crate::vm::eravm::deployers::EraVMDeployer;
 use crate::vm::eravm::EraVM;
@@ -206,7 +206,7 @@ impl Input {
         instances: &BTreeMap<String, Instance>,
         last_source: &str,
         caller: &web3::types::Address,
-        target: &target::Target,
+        target: &Target,
     ) -> anyhow::Result<Option<Self>> {
         let main_contract_instance = instances
             .values()
