316e0438936ff548740ea98ce670660073e3758d
Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>
2024-09-12 12:42:27 -0300

    fix: remove `start_nonce_from` variable from EVMAddressIterator (#95)

--- compiler_tester/src/vm/evm/address_iterator.rs
+++ compiler_tester/src/vm/evm/address_iterator.rs
@@ -14,18 +14,15 @@ use crate::vm::address_iterator::AddressIterator;
 pub struct EVMAddressIterator {
     /// The accounts create nonces.
     nonces: HashMap<web3::types::Address, usize>,
-    /// Whether to start nonce from zero.
-    start_nonce_from: usize,
 }
 
 impl EVMAddressIterator {
     ///
     /// A shortcut constructor.
     ///
-    pub fn new(start_nonce_from: usize) -> Self {
+    pub fn new() -> Self {
         Self {
             nonces: HashMap::new(),
-            start_nonce_from,
         }
     }
 }
@@ -56,11 +53,11 @@ impl AddressIterator for EVMAddressIterator {
     }
 
     fn increment_nonce(&mut self, caller: &web3::types::Address) {
-        let nonce = self.nonces.entry(*caller).or_insert(self.start_nonce_from);
+        let nonce = self.nonces.entry(*caller).or_insert(1);
         *nonce += 1;
     }
 
     fn nonce(&mut self, caller: &web3::types::Address) -> usize {
-        *self.nonces.entry(*caller).or_insert(self.start_nonce_from)
+        *self.nonces.entry(*caller).or_insert(1)
     }
 }
