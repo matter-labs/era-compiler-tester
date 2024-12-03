21393b8222ed81bdfc536c8a79953058df425b89
Igor Zhirkov <iz@matterlabs.dev>
2024-12-03 19:38:37 +0100

    refac: comment

--- compiler_tester/src/test/context/input.rs
+++ compiler_tester/src/test/context/input.rs
@@ -9,7 +9,10 @@ use super::case::CaseContext;
 ///
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct InputContext<'a> {
+    /// Context of the parent case, which contains this input.
     pub case_context: &'a CaseContext<'a>,
+    /// Optional name of the case.
     pub case_name: &'a Option<String>,
+    /// Index of the input in the case's array of inputs.
     pub selector: usize,
 }
