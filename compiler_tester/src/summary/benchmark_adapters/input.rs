450d722873a99a69b87c8db0a1f43e8370703a44
Igor Zhirkov <iz@matterlabs.dev>
2024-12-03 19:44:49 +0100

    refac: use Into for conversion

--- compiler_tester/src/summary/benchmark_adapters/input.rs
+++ compiler_tester/src/summary/benchmark_adapters/input.rs
@@ -4,24 +4,26 @@
 
 use crate::test::case::input::identifier::InputIdentifier;
 
-///
-/// Converts `[InputIdentifier]` to the representation used by the benchmark.
-///
-pub fn convert_input(input: InputIdentifier) -> benchmark_analyzer::Input {
-    match input {
-        InputIdentifier::Deployer {
-            contract_identifier,
-        } => benchmark_analyzer::Input::Deployer {
-            contract_identifier,
-        },
-        InputIdentifier::Runtime { input_index, name } => {
-            benchmark_analyzer::Input::Runtime { input_index, name }
-        }
-        InputIdentifier::StorageEmpty { input_index } => {
-            benchmark_analyzer::Input::StorageEmpty { input_index }
-        }
-        InputIdentifier::Balance { input_index } => {
-            benchmark_analyzer::Input::Balance { input_index }
+impl Into<benchmark_analyzer::Input> for InputIdentifier {
+    ///
+    /// Converts `[InputIdentifier]` to the representation used by the benchmark.
+    ///
+    fn into(self) -> benchmark_analyzer::Input {
+        match self {
+            InputIdentifier::Deployer {
+                contract_identifier,
+            } => benchmark_analyzer::Input::Deployer {
+                contract_identifier,
+            },
+            InputIdentifier::Runtime { input_index, name } => {
+                benchmark_analyzer::Input::Runtime { input_index, name }
+            }
+            InputIdentifier::StorageEmpty { input_index } => {
+                benchmark_analyzer::Input::StorageEmpty { input_index }
+            }
+            InputIdentifier::Balance { input_index } => {
+                benchmark_analyzer::Input::Balance { input_index }
+            }
         }
     }
 }
--- compiler_tester/src/summary/benchmark_adapters/selector.rs
+++ compiler_tester/src/summary/benchmark_adapters/selector.rs
@@ -4,13 +4,11 @@
 
 use crate::test::selector::TestSelector;
 
-use super::input::convert_input;
-
 ///
 /// Converts `[TestSelector]` to the representation used by the benchmark.
 ///
 pub fn convert_selector(selector: TestSelector) -> benchmark_analyzer::TestSelector {
     let TestSelector { path, case, input } = selector;
-    let input = input.map(convert_input);
+    let input = input.map(Into::into);
     benchmark_analyzer::TestSelector { path, case, input }
 }
