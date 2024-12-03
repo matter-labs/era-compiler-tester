4609e282cb25dfa2d6e6310b9834261d73233c4d
Igor Zhirkov <iz@matterlabs.dev>
2024-12-04 00:23:06 +0100

    refac: comment

--- benchmark_analyzer/src/benchmark/format/mod.rs
+++ benchmark_analyzer/src/benchmark/format/mod.rs
@@ -7,9 +7,13 @@ pub mod json;
 
 use crate::benchmark::Benchmark;
 
+///
 /// Serialization format for benchmark data.
+///
 pub trait IBenchmarkSerializer {
     type Err: std::error::Error;
+    ///
     /// Serialize benchmark data in the selected format.
+    ///
     fn serialize_to_string(&self, benchmark: &Benchmark) -> anyhow::Result<String, Self::Err>;
 }
