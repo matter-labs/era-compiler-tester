54768d5b49630bf5313afe4745d72b69abea30a7
Igor Zhirkov <iz@matterlabs.dev>
2025-01-06 15:01:28 +0100

    refac: make `target` a enum in benchmark context

--- benchmark_analyzer/src/model/context/mod.rs
+++ benchmark_analyzer/src/model/context/mod.rs
@@ -5,12 +5,12 @@
 ///
 /// A context for benchmarking, passed by compiler-tester.
 ///
-#[derive(Clone, Debug, Default, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
 pub struct Context {
     /// Unique identifier of the machine.
     pub machine: String,
-    /// Target, for example as "eravm" or "evm".
-    pub target: String,
+    /// Target, for example "eravm" or "evm".
+    pub target: era_compiler_common::Target,
     /// Type of `solc`, for example `zksync`
     pub solc_type: String,
 }
@@ -21,15 +21,12 @@ pub struct Context {
 pub fn validate_context(context: &Context) -> anyhow::Result<()> {
     let Context {
         machine,
-        target,
         solc_type,
+        target: _
     } = context;
 
     if machine.is_empty() {
         anyhow::bail!("The `machine` field in the benchmark context is empty")
-    }
-    if target.is_empty() {
-        anyhow::bail!("The `target` field in the benchmark context is empty")
     }
     if solc_type.is_empty() {
         anyhow::bail!("The `solc_type` field in the benchmark context is empty")
--- benchmark_analyzer/src/output/format/json/lnt/benchmark/machine.rs
+++ benchmark_analyzer/src/output/format/json/lnt/benchmark/machine.rs
@@ -12,7 +12,7 @@ pub struct Machine {
     /// Machine name, for example "LNT-AArch64-A53-O3__clang_DEV__aarch64".
     pub name: String,
     /// Target name, for example "eravm" or "solc".
-    pub target: String,
+    pub target: era_compiler_common::Target,
     /// Optimizations level, for example "+M3B3".
     pub optimizations: String,
     /// Type of solc, for example, "zksync".
